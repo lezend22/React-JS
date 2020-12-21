7208e61d5a4ad724a4832e720be21aa0b241462f
lezend22 <lezend22@gmail.com>
2020-12-21 18:01:21 +0900

    active test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; //you can put more component in application
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "ramen",
+    rating: 4.0,
+  },
+  {
+    id: 3,
+    name: "udon",
+    rating: 3.5,
+  },
+];
+
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h3>{rating} / 5.0</h3>
+    </div>
+  );
+}
+
+Food.potato = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
+      ))}
     </div>
   );
 }

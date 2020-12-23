02851e9040fc320f624819dd7f584272caf4bf1c
lezend22 <lezend22@gmail.com>
2020-12-24 05:06:56 +0900

    router details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react"; //you can put more component in application
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom"; //also can use Browser Router instead but hard to adopt in gh pages
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
+      <Route path="/movie-detail" component={Detail}></Route>
     </HashRouter>
   );
 }

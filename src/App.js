07f9f534ef7efc7c12852a166092f70fd67d75dc
lezend22 <lezend22@gmail.com>
2020-12-24 03:00:34 +0900

    add routers

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 import React from "react"; //you can put more component in application
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMoives = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  // this.setState(movies : movies) one from state, one from json
-  // potato.data.data.movies >> es6 form {data :{data:{movies}}}
-
-  componentDidMount() {
-    this.getMoives();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="Loading">
-            <span className="Loading__text">Loading,,,</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
+  );
 }
 
 export default App;
